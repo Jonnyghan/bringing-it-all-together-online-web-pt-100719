da46a3017b6d8e0edede482ff710ddadad5e6801
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-16 00:54:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,8 @@ class Dog
     INSERT INTO dogs(name,breed) 
     VALUES (?,?)
     SQL
-    DB[:conn].execute(sql, self.name,self.breed)  
+    DB[:conn].execute(sql, self.name,self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_row_id() FROM dogs")[0][0]
   end
   
 end
