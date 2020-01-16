0650e8e4d637fe2b28f8ee3de0cb8cfcc0d9b28c
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-16 01:35:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,11 +45,10 @@ class Dog
   end
   
   def self.new_from_db(row)
-   # binding.pry
     hash = {
-      :id = row[0],
-      :name = row[1],
-      :breed = row[2]}
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]}
     self.new(hash)
   end
   
